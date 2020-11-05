4ebcb7eddd32ac40c1e245dd4765ca64d67f4379
lzj1769 <lzj1769@gmail.com>
2020-11-05 16:48:06 +0100

    add cv

--- scopen/Main.py
+++ scopen/Main.py
@@ -2,6 +2,8 @@ import time
 from datetime import datetime
 import argparse
 from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.utils import check_array
 
 from .MF import NMF, _beta_divergence
 from .Utils import *
@@ -23,24 +25,17 @@ def parse_args():
                         help="Input format. Currently available: sparse, dense, 10X, 10Xh5."
                              "Default: dense")
     parser.add_argument("--n_components", type=int, default=30, help="Number of components. Default: 30")
-    parser.add_argument("--n_neighbors", type=int, default=1, help="Number of neighbors used for dropout calculation. "
-                                                                   "Default: 1")
     parser.add_argument("--max_iter", type=int, default=500)
-    parser.add_argument("--random_state", type=int, default=42)
-    parser.add_argument("--rho", type=float, default=None,
-                        help='If set, will use this number as dropout rate.'
-                             'Default: None')
-    parser.add_argument("--binarize", default=False,
-                        action='store_true',
-                        help='If set, the imputed matrix will be sparsified based on thresholds by keeping quantile.'
-                             'Default: False')
+    parser.add_argument("--random_state", type=int, default=42, help="Random state. Default: 42")
+    parser.add_argument("--knn_impute", default=False, action='store_true',
+                        help="If set, the matrix will first be imputed by using k-nearest neighbors"
+                             "Default: False")
+    parser.add_argument("--n_neighbors", type=int, default=1, help="Number of neighbors used for knn imputation. "
+                                                                   "Default: 1")
     parser.add_argument("--estimate_rank", default=False,
                         action='store_true',
                         help='If set, the number of components will be determined by using cross validation.'
                              'Default: False')
-    parser.add_argument("--quantile", type=float, default=None,
-                        help='If set, will use this number to binarize the imputed matrix.'
-                             'must be between 0 and 1. Default: None')
     parser.add_argument("--alpha", type=float, default=1)
     parser.add_argument("--verbose", type=int, default=0)
 
@@ -50,46 +45,71 @@ def parse_args():
     return parser.parse_args()
 
 
-def compute_rho_by_knn(data, k):
-    neigh = NearestNeighbors(n_neighbors=k, algorithm='auto', n_jobs=-1, metric='jaccard')
-    neigh.fit(np.transpose(data))
+def knn_impute(data, n_neighbors):
+    model = TfidfTransformer(smooth_idf=False)
+    tf_idf = model.fit_transform(np.transpose(data))
+
+    neigh = NearestNeighbors(n_neighbors=n_neighbors,
+                             n_jobs=-1,
+                             metric='cosine')
+    neigh.fit(tf_idf)
     indices = neigh.kneighbors(return_distance=False)
 
-    data_y = np.zeros(data.shape)
-    rho = np.zeros(indices.shape[0])
+    data_y = np.zeros(data.shape, dtype=np.int)
     for i in range(indices.shape[0]):
-        _y = np.greater(np.sum(data[:, indices[i]], axis=1), 0)
+        _y = np.sum(data[:, indices[i]], axis=1)
         data_y[:, i] = np.greater(data[:, i] + _y, 0)
-        rho[i] = (np.count_nonzero(data_y[:, i]) - np.count_nonzero(data[:, i])) / np.count_nonzero(data_y[:, i])
 
-    return rho, data_y
+    return data_y
+
+
+def train_test_split(data, args):
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, splitting data for cross validation...")
+    m, n = data.shape
+
+    # create test data by setting 10% elements to NAN
+    test_mask = np.empty(shape=(m, n), dtype=np.bool)
+
+    # set seed for reproducibility
+    np.random.seed(args.random_state)
+    for i in range(m):
+        test_mask[i, :] = np.random.choice([True, False], n, p=[0.1, 0.9])
+
+    data_train, data_test, = data.copy(), data.copy()
+    data_train[test_mask] = np.nan
+    data_test[~test_mask] = np.nan
+
+    data_train = check_array(data_train, accept_sparse=('csr', 'csc'), dtype=float,
+                             force_all_finite=False)
+
+    data_test = check_array(data_test, accept_sparse=('csr', 'csc'), dtype=float,
+                            force_all_finite=False)
+
+    return data_train, data_test
+
+
+def select_model(n_components_list, alpha_list, data, args):
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+          f"running model selection...")
+
+    data_train, data_test = train_test_split(data=data, args=args)
+
+    num_train = np.count_nonzero(~np.isnan(data_train))
+    num_test = np.count_nonzero(~np.isnan(data_test))
+
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+          f"training data: {num_train}, test data: {num_test}")
 
 
 def main():
     args = parse_args()
 
-    if args.binarize:
-        assert args.quantile is not None, "please provide a number to compute the q-th quantile"
-
     start = time.time()
-
-    data, barcodes, peaks = None, None, None
 
     print(args)
 
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, loading data...")
-    if args.input_format == "sparse":
-        data, barcodes, peaks = get_data_from_sparse_file(filename=args.input)
-
-    elif args.input_format == "dense":
-        data, barcodes, peaks = get_data_from_dense_file(filename=args.input)
-
-    elif args.input_format == "10Xh5":
-        data, barcodes, peaks = get_data_from_10x_h5(filename=args.input)
-
-    elif args.input_format == "10X":
-        data, barcodes, peaks = get_data_from_10x(input_dir=args.input)
-
+    data, barcodes, peaks = load_data(args=args)
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, data loaded...")
 
     data = np.greater(data, 0)
@@ -102,48 +122,30 @@ def main():
     n_open_regions = np.count_nonzero(data, axis=0)
     plot_open_regions_density(n_open_regions, args)
 
-    if args.rho is None:
-        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, estimating dropout rate...")
-        rho, data_y = compute_rho_by_knn(data, k=args.n_neighbors)
-        filename = os.path.join(args.output_dir, "{}_y.txt".format(args.output_prefix))
-        write_data_to_dense_file(filename=filename, data=data_y, barcodes=barcodes, peaks=peaks)
+    if args.knn_impute:
+        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, applying knn imputation...")
+        data = knn_impute(data=data, n_neighbors=args.n_neighbors)
+        filename = os.path.join(args.output_dir, "{}_knn.txt".format(args.output_prefix))
+        write_data_to_dense_file(filename=filename, data=data, barcodes=barcodes, peaks=peaks)
+        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, knn imputation applied!")
 
-        plot_estimated_dropout(rho=rho, args=args)
-        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, dropout rate estimated!")
+    # tf-idf
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, applying tf-idf transformation...")
+    model = TfidfTransformer(smooth_idf=False)
+    tf_idf = np.transpose(model.fit_transform(np.transpose(data)))
 
-        df = pd.DataFrame({'num_peaks': n_open_regions,
-                           'estimated_dropout': rho,
-                           'barcodes': barcodes})
+    if sp_sparse.issparse(tf_idf):
+        tf_idf = tf_idf.toarray()
 
-        filename = os.path.join(args.output_dir, "{}_stat.txt".format(args.output_prefix))
-        df.to_csv(filename, index=False, sep="\t")
+    filename = os.path.join(args.output_dir, "{}_tf_idf.txt".format(args.output_prefix))
+    write_data_to_dense_file(filename=filename, data=tf_idf, barcodes=barcodes, peaks=peaks)
 
-    else:
-        rho = args.rho
-
-    data = data[:, :] * (1 / (1 - rho))
-
-    filename = os.path.join(args.output_dir, "{}_x.txt".format(args.output_prefix))
-    write_data_to_dense_file(filename=filename, data=data, barcodes=barcodes, peaks=peaks)
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, tf-idf transformation applied...")
 
     if args.estimate_rank:
         print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, estimating ranks...")
         # create test data by setting 10% elements to NAN
         test_mask = np.empty(shape=(m, n), dtype=np.bool)
-        # set seed for reproducibility
-        np.random.seed(args.random_state)
-
-        for i in range(m):
-            test_mask[i, :] = np.random.choice([True, False], n, p=[0.1, 0.9])
-
-        data_train = data.copy()
-        data_train[test_mask] = np.nan
-
-        num_train = np.count_nonzero(~np.isnan(data_train))
-        num_test = np.count_nonzero(np.isnan(data_train))
-
-        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-              f"training data: {num_train}, test data: {num_test}")
 
         n_components_list = [5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
         train_error_list, test_error_list = [], []
@@ -164,12 +166,13 @@ def main():
             w_hat = model.fit_transform(data_train)
             h_hat = model.components_
 
-            train_error = _beta_divergence(np.ma.masked_array(data, test_mask),
-                                           w_hat, h_hat, beta=2,
+            train_error = _beta_divergence(data_train,
+                                           w_hat, h_hat, beta='frobenius',
                                            square_root=True)
 
-            test_error = _beta_divergence(np.ma.masked_array(data, ~test_mask),
-                                          w_hat, h_hat, beta=2, square_root=True)
+            test_error = _beta_divergence(data_test,
+                                          w_hat, h_hat, beta='frobenius',
+                                          square_root=True)
 
             print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
                   f"ranks: {n_components}, training error: {train_error}, test error: {test_error}")
@@ -201,29 +204,11 @@ def main():
                     beta_loss="frobenius",
                     solver="cd")
 
-        best_w_hat = model.fit_transform(data)
+        best_w_hat = model.fit_transform(tf_idf)
         best_h_hat = model.components_
 
-    del data
-    m_hat = np.dot(best_w_hat, best_h_hat)
-    np.clip(m_hat, 0, 1, out=m_hat)
-
-    if args.binarize:
-        threshold = np.quantile(m_hat, args.quantile, axis=0)
-        m_hat = (m_hat > threshold).astype(int)
-
-    output_wh(best_w_hat, best_h_hat, peaks, barcodes, args)
-
-    if args.output_format == "sparse":
-        filename = os.path.join(args.output_dir, "{}.txt".format(args.output_prefix))
-        write_data_to_sparse_file(filename=filename, data=m_hat, barcodes=barcodes, peaks=peaks)
-
-    elif args.output_format == "dense":
-        filename = os.path.join(args.output_dir, "{}.txt".format(args.output_prefix))
-        write_data_to_dense_file(filename=filename, data=m_hat, barcodes=barcodes, peaks=peaks)
-
-    elif args.output_format == "10X":
-        write_data_to_10x(output_dir=args.output_dir, data=m_hat, barcodes=barcodes, peaks=peaks)
+    save_data(w=best_w_hat, h=best_h_hat,
+              peaks=peaks, barcodes=barcodes, args=args)
 
     secs = time.time() - start
     m, s = divmod(secs, 60)
