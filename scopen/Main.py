f49a741c8404530febcfd303f77224d5d46d76ac
lzj1769 <lzj1769@gmail.com>
2020-11-12 12:00:05 +0100

    add

--- scopen/Main.py
+++ scopen/Main.py
@@ -3,6 +3,8 @@ import argparse
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.utils import check_array
+from multiprocessing import Pool, cpu_count
+from kneed import KneeLocator
 
 from .MF import NMF, _beta_divergence
 from .Utils import *
@@ -30,16 +32,22 @@ def parse_args():
     parser.add_argument("--knn_impute", default=False, action='store_true',
                         help="If set, the matrix will first be imputed by using k-nearest neighbors"
                              "Default: False")
-    parser.add_argument("--n_neighbors", type=int, default=1,
+    parser.add_argument("--n_neighbors", type=int, default=5,
                         help="Number of neighbors used for knn imputation. "
-                             "Default: 1")
+                             "Default: 5")
     parser.add_argument("--select_model", default=False,
                         action='store_true',
                         help='If set, both number of components and alpha will be determined by using cross validation.'
                              'Default: False')
-    parser.add_argument("--n_components_list", default='5,6,7,8,9,10,15,20,25,30',
-                        help='Components number list for model selection.'
-                             'Default: 5, 6, 7, 8, 9, 10, 15, 20, 25, 30')
+    parser.add_argument("--min_n_components", type=int, default=2,
+                        help="Minimum of number of components for model selection, must be positive."
+                             "Default: 2")
+    parser.add_argument("--max_n_components", type=int, default=30,
+                        help="Minimum of number of components for model selection, must be positive."
+                             "Default: 2")
+    parser.add_argument("--step_n_components", type=int, default=1,
+                        help="Spacing between values"
+                             "Default: 1")
     parser.add_argument("--alpha_list", default='0.001,0.01,0.1,1',
                         help='Alpha list for model selection.'
                              'Default: 0.001, 0.01, 0.1, 1')
@@ -47,6 +55,8 @@ def parse_args():
                         help="Proportion of data for model selection, should be between 0 and 1."
                              "Default: 0.1")
     parser.add_argument("--random_state", type=int, default=42, help="Random state. Default: 42")
+    parser.add_argument("--nc", type=int, metavar="INT", default=1,
+                        help="The number of cores. DEFAULT: 1")
     parser.add_argument("--verbose", type=int, default=0)
 
     version_message = "Version: " + str(__version__)
@@ -57,28 +67,29 @@ def parse_args():
 
 def knn_impute(data, n_neighbors):
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"applying knn imputation...")
-
-    model = TfidfTransformer(smooth_idf=False)
-    tf_idf = model.fit_transform(np.transpose(data))
+          f"running knn imputation...")
 
     neigh = NearestNeighbors(n_neighbors=n_neighbors,
                              n_jobs=-1,
                              metric='cosine')
-    neigh.fit(tf_idf)
+    neigh.fit(np.transpose(data))
     indices = neigh.kneighbors(return_distance=False)
 
-    data_y = np.zeros(data.shape, dtype=np.int)
+    data_impute = data.copy()
+
     for i in range(indices.shape[0]):
         _y = np.sum(data[:, indices[i]], axis=1)
-        data_y[:, i] = np.greater(data[:, i] + _y, 0)
 
-    return data_y
+        idx = (data[:, i] == 0) & (_y > 0)
+        # set missing elements to NA
+        data_impute[idx, i] = np.nan
+
+    return data_impute
 
 
 def tf_idf_transform(data):
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"applying tf-idf transformation...")
+          f"running tf-idf transformation...")
     model = TfidfTransformer(smooth_idf=False)
     tf_idf = np.transpose(model.fit_transform(np.transpose(data)))
 
@@ -88,40 +99,13 @@ def tf_idf_transform(data):
     return tf_idf
 
 
-def train_test_split(data, args):
-    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"splitting data for cross validation...")
-    m, n = data.shape
-
-    # create test data by setting 10% elements to NAN
-    test_mask = np.empty(shape=(m, n), dtype=np.bool)
-
-    # set seed for reproducibility
-    np.random.seed(args.random_state)
-    for i in range(m):
-        test_mask[i, :] = np.random.choice([True, False], n, p=[args.test_size, 1 - args.test_size])
-
-    data_train, data_test, = data.copy(), data.copy()
-    data_train[test_mask] = np.nan
-    data_test[~test_mask] = np.nan
-
-    data_train = check_array(data_train, accept_sparse=('csr', 'csc'), dtype=float,
-                             force_all_finite=False)
-
-    data_test = check_array(data_test, accept_sparse=('csr', 'csc'), dtype=float,
-                            force_all_finite=False)
-
-    num_train = np.count_nonzero(~np.isnan(data_train))
-    num_test = np.count_nonzero(~np.isnan(data_test))
-
-    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"training data: {num_train}, test data: {num_test}")
-
-    return data_train, data_test
-
-
 def run_nmf(data, n_components, alpha, max_iter, verbose, random_state,
-            beta_loss, solver, init=None, w=None, h=None):
+            beta_loss, w=None, h=None):
+    if np.any(np.isnan(data)):
+        solver, init = "mu", "random"
+    else:
+        solver, init = "cd", "nndsvd"
+
     model = NMF(n_components=n_components,
                 random_state=random_state,
                 alpha=alpha,
@@ -135,56 +119,40 @@ def run_nmf(data, n_components, alpha, max_iter, verbose, random_state,
     w_hat = model.fit_transform(X=data, W=w, H=h)
     h_hat = model.components_
 
-    return w_hat, h_hat
+    return w_hat, h_hat, model.reconstruction_err_
 
 
 def select_model(data, args):
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
           f"running model selection...")
 
-    data_train, data_test = train_test_split(data=data, args=args)
+    n_components_list = np.arange(start=args.min_n_components,
+                                  stop=args.max_n_components + 1,
+                                  step=args.step_n_components)
 
-    w_best, h_best, rank_best, alpha_best = None, None, None, None
-    best_error = np.inf
-
-    n_components_list = list(map(int, args.n_components_list.split(',')))
-    alpha_list = list(map(float, args.alpha_list.split(',')))
-
-    print(n_components_list)
-    print(alpha_list)
-
+    w_hat_dict, h_hat_dict, error_list = dict(), dict(), list()
     for n_components in n_components_list:
-        for alpha in alpha_list:
-            w_hat, h_hat = run_nmf(data=data_train,
-                                   n_components=n_components,
-                                   alpha=alpha,
-                                   max_iter=args.max_iter,
-                                   verbose=args.verbose,
-                                   random_state=args.random_state,
-                                   beta_loss="frobenius",
-                                   solver="mu",
-                                   init="random")
+        w_hat, h_hat, _error = run_nmf(data=data,
+                                       n_components=n_components,
+                                       alpha=args.alpha,
+                                       max_iter=args.max_iter,
+                                       verbose=args.verbose,
+                                       random_state=args.random_state,
+                                       beta_loss="frobenius")
 
-            train_error = _beta_divergence(data_train, w_hat, h_hat,
-                                           beta='frobenius',
-                                           square_root=True)
+        w_hat_dict[n_components] = w_hat
+        h_hat_dict[n_components] = h_hat
+        error_list.append(_error)
 
-            test_error = _beta_divergence(data_test, w_hat, h_hat,
-                                          beta='frobenius',
-                                          square_root=True)
+        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+              f"ranks: {n_components}, training error: {_error}")
 
-            print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-                  f"ranks: {n_components}, alpha: {alpha}, "
-                  f"training error: {train_error}, test error: {test_error}")
+    kl = KneeLocator(n_components_list, error_list,
+                     S=1.0, curve="convex", direction="decreasing")
 
-            if test_error < best_error:
-                best_error = test_error
-                w_best, h_best, rank_best, alpha_best = w_hat, h_hat, n_components, alpha
+    plot_knee(kl, args)
 
-    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"model selection done, best rank: {rank_best}, best alpha: {alpha_best}")
-
-    return w_best, h_best, rank_best, alpha_best
+    return w_hat_dict[kl.knee], h_hat_dict[kl.knee]
 
 
 def main():
@@ -192,6 +160,9 @@ def main():
 
     args = parse_args()
     print(args)
+
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+          f"detected {cpu_count()} cpus, {args.nc} of them are used.")
 
     data, barcodes, peaks = load_data(args=args)
 
@@ -207,25 +178,25 @@ def main():
 
     plot_open_regions_density(data, args)
 
-    if args.knn_impute:
-        data = knn_impute(data=data, n_neighbors=args.n_neighbors)
-
     # tf-idf
     tf_idf = tf_idf_transform(data)
 
+    if args.knn_impute:
+        tf_idf = knn_impute(data=tf_idf, n_neighbors=args.n_neighbors)
+
     if args.select_model:
-        w_hat, h_hat, rank_best, alpha_best = select_model(data=tf_idf,
-                                                           args=args)
+        w_hat, h_hat = select_model(data=tf_idf, args=args)
 
     else:
-        w_hat, h_hat = run_nmf(data=tf_idf,
-                               n_components=args.n_components,
-                               alpha=args.alpha,
-                               max_iter=args.max_iter,
-                               verbose=args.verbose,
-                               random_state=args.random_state,
-                               beta_loss="frobenius",
-                               solver="cd")
+        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+              f"running NMF...")
+        w_hat, h_hat, _error = run_nmf(data=tf_idf,
+                                       n_components=args.n_components,
+                                       alpha=args.alpha,
+                                       max_iter=args.max_iter,
+                                       verbose=args.verbose,
+                                       random_state=args.random_state,
+                                       beta_loss="frobenius")
 
     save_data(w=w_hat, h=h_hat, peaks=peaks, barcodes=barcodes, args=args)
 
@@ -233,7 +204,8 @@ def main():
     m, s = divmod(secs, 60)
     h, m = divmod(m, 60)
 
-    print(f"Done, total time: {h: .2f}h {m: .2f}m {s: .2f}s")
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+          f"total time: {h: .2f}h {m: .2f}m {s: .2f}s")
 
 
 if __name__ == '__main__':
