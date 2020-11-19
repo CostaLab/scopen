2272fb2f0a46652e8cd201e24f5dfbf133ea411b
lzj1769 <lzj1769@gmail.com>
2020-11-19 15:43:45 +0100

    add

--- scopen/Main.py
+++ scopen/Main.py
@@ -15,7 +15,7 @@ def parse_args():
     parser.add_argument("--input", type=str, default=None,
                         help="Input name, can be a file name or a directory")
     parser.add_argument("--input_format", type=str, default='dense',
-                        help="Input format. Currently available: sparse, dense, 10X."
+                        help="Input format. Currently available: sparse, dense, 10X. "
                              "Default: dense")
     parser.add_argument("--output_dir", type=str, default=os.getcwd(),
                         help="If specified, all output files will be written to that directory. "
@@ -23,20 +23,20 @@ def parse_args():
     parser.add_argument("--output_prefix", type=str, default=None,
                         help="Output filename")
     parser.add_argument("--output_format", type=str, default='dense',
-                        help="Input format. Currently available: sparse, dense, 10X, 10Xh5."
+                        help="Input format. Currently available: sparse, dense, 10X, 10Xh5. "
                              "Default: dense")
-    parser.add_argument("--n_components", type=int, default=30, help="Number of components. Default: 30")
-    parser.add_argument("--alpha", type=float, default=1)
-    parser.add_argument("--max_iter", type=int, default=500)
-    parser.add_argument("--knn_impute", default=False, action='store_true',
-                        help="If set, the matrix will first be imputed by using k-nearest neighbors"
-                             "Default: False")
-    parser.add_argument("--n_neighbors", type=int, default=1,
-                        help="Number of neighbors used for knn-based dropout rate estimation. "
+    parser.add_argument("--n_components", type=int, default=30,
+                        help="Number of components. "
+                             "Default: 30")
+    parser.add_argument("--alpha", type=float, default=1,
+                        help="Parameter for model regularization to prevent from over-fitting. "
                              "Default: 1")
+    parser.add_argument("--max_iter", type=int, default=500,
+                        help="Number of iteration for optimization. "
+                             "Default: 500")
     parser.add_argument("--select_model", default=False,
                         action='store_true',
-                        help='If set, both number of components and alpha will be determined by using cross validation.'
+                        help='If set, number of components will be selected by knee point. '
                              'Default: False')
     parser.add_argument("--min_n_components", type=int, default=2,
                         help="Minimum of number of components for model selection, must be positive."
@@ -47,13 +47,18 @@ def parse_args():
     parser.add_argument("--step_n_components", type=int, default=1,
                         help="Spacing between values"
                              "Default: 1")
-    parser.add_argument("--alpha_list", default='0.001,0.01,0.1,1',
-                        help='Alpha list for model selection.'
-                             'Default: 0.001, 0.01, 0.1, 1')
-    parser.add_argument("--rho", type=float, default=None,
-                        help='Dropout rate per cell.'
-                             'Default: None')
-    parser.add_argument("--random_state", type=int, default=42, help="Random state. Default: 42")
+    parser.add_argument("--rho", type=float, default=0.9,
+                        help='Dropout rate per cell, must between 0 and 1. '
+                             'Default: 0.9')
+    parser.add_argument("--estimate_rho", default=False,
+                        action='store_true',
+                        help='If set, dropout rate will be estimated based on knn. '
+                             'Default: False')
+    parser.add_argument("--n_neighbors", type=int, default=1,
+                        help="Number of neighbors used for knn-based dropout rate estimation. "
+                             "Default: 1")
+    parser.add_argument("--random_state", type=int, default=42,
+                        help="Random state. Default: 42")
     parser.add_argument("--nc", type=int, metavar="INT", default=1,
                         help="The number of cores. DEFAULT: 1")
     parser.add_argument("--verbose", type=int, default=0)
@@ -81,56 +86,33 @@ def compute_rho_by_knn(data, k):
     return rho, data_y
 
 
-def knn_impute(data, n_neighbors):
-    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-          f"running knn imputation...")
-
-    neigh = NearestNeighbors(n_neighbors=n_neighbors,
-                             n_jobs=-1,
-                             metric='cosine')
-    neigh.fit(np.transpose(data))
-    indices = neigh.kneighbors(return_distance=False)
-
-    data_impute = data.copy()
-
-    for i in range(indices.shape[0]):
-        _y = np.sum(data[:, indices[i]], axis=1)
-
-        idx = (data[:, i] == 0) & (_y > 0)
-        # set missing elements to NA
-        data_impute[idx, i] = np.nan
-
-    return data_impute
-
-
 def tf_idf_transform(data):
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
           f"running tf-idf transformation...")
     model = TfidfTransformer(smooth_idf=False)
     tf_idf = np.transpose(model.fit_transform(np.transpose(data)))
 
-    if sp_sparse.issparse(tf_idf):
-        tf_idf = tf_idf.toarray()
-
     return tf_idf
 
 
-def run_nmf(data, n_components, alpha, max_iter, verbose, random_state,
-            beta_loss, w=None, h=None):
+def run_nmf(arguments):
+    data, n_components, alpha, max_iter, verbose, random_state = arguments
     model = NMF(n_components=n_components,
                 random_state=random_state,
                 alpha=alpha,
                 l1_ratio=0,
                 max_iter=max_iter,
                 verbose=verbose,
-                beta_loss=beta_loss,
                 solver="cd",
                 init="nndsvd")
 
-    w_hat = model.fit_transform(X=data, W=w, H=h)
+    w_hat = model.fit_transform(X=data)
     h_hat = model.components_
 
-    return w_hat, h_hat, model.reconstruction_err_
+    print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
+          f"ranks: {n_components}, fitting error: {model.reconstruction_err_}")
+
+    return [w_hat, h_hat, model.reconstruction_err_]
 
 
 def select_model(data, args):
@@ -142,21 +124,30 @@ def select_model(data, args):
                                   step=args.step_n_components)
 
     w_hat_dict, h_hat_dict, error_list = dict(), dict(), list()
-    for n_components in n_components_list:
-        w_hat, h_hat, _error = run_nmf(data=data,
-                                       n_components=n_components,
-                                       alpha=args.alpha,
-                                       max_iter=args.max_iter,
-                                       verbose=args.verbose,
-                                       random_state=args.random_state,
-                                       beta_loss="frobenius")
+    if args.nc == 1:
+        for n_components in n_components_list:
+            arguments = (data, n_components, args.alpha,
+                         args.max_iter, args.verbose,
+                         args.random_state)
 
-        w_hat_dict[n_components] = w_hat
-        h_hat_dict[n_components] = h_hat
-        error_list.append(_error)
+            res = run_nmf(arguments)
+            w_hat_dict[n_components] = res[0]
+            h_hat_dict[n_components] = res[1]
+            error_list.append(res[2])
 
-        print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
-              f"ranks: {n_components}, training error: {_error}")
+    elif args.nc > 1:
+        arguments_list = list()
+        for n_components in n_components_list:
+            arguments = (data, n_components, args.alpha, args.max_iter, args.verbose, args.random_state)
+            arguments_list.append(arguments)
+
+        with Pool(processes=args.nc) as pool:
+            res = pool.map(run_nmf, arguments_list)
+
+        for i, n_components in enumerate(n_components_list):
+            w_hat_dict[n_components] = res[i][0]
+            h_hat_dict[n_components] = res[i][1]
+            error_list.append(res[i][2])
 
     kl = KneeLocator(n_components_list, error_list,
                      S=1.0, curve="convex", direction="decreasing")
@@ -167,17 +158,18 @@ def select_model(data, args):
 
 
 def main():
-    start = time.time()
-
     args = parse_args()
+    assert args.input is not None, "Please specify an input"
     print(args)
+
+    start = time.time()
 
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
           f"detected {cpu_count()} cpus, {args.nc} of them are used.")
 
     data, barcodes, peaks = load_data(args=args)
 
-    data = np.greater(data, 0)
+    # data = np.greater(data, 0)
     (m, n) = data.shape
 
     print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
@@ -189,7 +181,7 @@ def main():
 
     plot_open_regions_density(data, args)
 
-    if args.rho is None:
+    if args.estimate_rho:
         print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
               f"estimating dropout rate...")
         rho, data_y = compute_rho_by_knn(data, k=args.n_neighbors)
@@ -212,22 +204,18 @@ def main():
     # tf-idf
     tf_idf = tf_idf_transform(data)
 
-    if args.knn_impute:
-        tf_idf = knn_impute(data=tf_idf, n_neighbors=args.n_neighbors)
-
     if args.select_model:
         w_hat, h_hat = select_model(data=tf_idf, args=args)
 
     else:
         print(f"{datetime.now().strftime('%m/%d/%Y %H:%M:%S')}, "
               f"running NMF...")
-        w_hat, h_hat, _error = run_nmf(data=tf_idf,
-                                       n_components=args.n_components,
-                                       alpha=args.alpha,
-                                       max_iter=args.max_iter,
-                                       verbose=args.verbose,
-                                       random_state=args.random_state,
-                                       beta_loss="frobenius")
+        arguments = (data, args.n_components, args.alpha,
+                     args.max_iter, args.verbose,
+                     args.random_state)
+
+        res = run_nmf(arguments)
+        w_hat, h_hat = res[0], res[1]
 
     save_data(w=w_hat, h=h_hat, peaks=peaks, barcodes=barcodes, args=args)
 
