6055b9f7dad01fd66d0ef67d69586a1c6a3aa790
Zhijian Li <zhijian.li@rwth-aachen.de>
2021-04-06 14:40:35 +0200

    add

---
+++ reproducibility/Hematopoiesis/Merged/script/call_cluster.py
@@ -0,0 +1,7 @@
+import os
+
+job_name = "merge"
+command = "bsub -J " + job_name + " -o " + "./cluster_out/" + job_name + "_out.txt -e " + \
+          "./cluster_err/" + job_name + "_err.txt "
+command += "-W 48:00 -M 51200 -S 100 -R \"select[hpcwork]\" -P rwth0233 ./run.zsh "
+os.system(command)
